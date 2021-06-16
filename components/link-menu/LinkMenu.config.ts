e6ac515138f3e6dae7a36ce5c3170f5ffc51e579
Giuseppe Locanto <peplocanto@gmail.com>
2021-06-16 14:42:01 +0200

    add contact icons

--- components/link-menu/LinkMenu.config.ts
+++ components/link-menu/LinkMenu.config.ts
@@ -7,7 +7,7 @@ import {
   faMediumM,
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
-import { faCode, faFileAlt, faUniversity } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faEnvelope, faFileAlt, faPhoneAlt, faUniversity } from '@fortawesome/free-solid-svg-icons';
 import { LinkMenuItemProps } from './LinkMenu.model';
 
 export const LINK_MENU_CONFIG: Record<Pages, LinkMenuItemProps[]> = {
@@ -27,5 +27,9 @@ export const LINK_MENU_CONFIG: Record<Pages, LinkMenuItemProps[]> = {
     { icon: faFileAlt, label: 'links.resume', url: '/files/GiuseppeLocantoCV.pdf' },
     { icon: faUniversity, label: 'links.studies', url: 'https://trello.com/peplocantocourses' },
   ],
-  [Pages.CONTACTS]: [],
+  [Pages.CONTACTS]: [
+    { icon: faLinkedinIn, label: 'links.linkedin', url: 'https://www.linkedin.com/in/peplocanto/' },
+    { icon: faPhoneAlt, label: 'links.phone', url: 'tel:+34621040209' },
+    { icon: faEnvelope, label: 'links.mail', url: 'mailto:pep@peplocanto.com' }
+  ],
 };
